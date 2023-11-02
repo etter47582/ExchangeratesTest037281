1f0c0033e3835e0bb705795e4836f2825697e079
vladislav.veretennik <vladislav.veretennikov@geniusto.com>
2023-11-02 22:19:41 +0200

    initial commit, add main vc

---
+++ ExchangeratesTest037281/ExchangeratesTest037281/CurrencyConverterVC/CurrencyConverterVC.swift
@@ -0,0 +1,20 @@
+//
+//  CurrencyConverterVC.swift
+//  ExchangeratesTest037281
+//
+//  Created by Vladyslav Veretennikov on 02.11.2023.
+//
+
+import UIKit
+
+class CurrencyConverterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+}
